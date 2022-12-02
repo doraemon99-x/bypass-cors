f115c697de7b40d17fd3973f3c0743d47b1acee5
doraemon99-x <75873265+doraemon99-x@users.noreply.github.com>
2022-12-02 15:52:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 corsProxy.createServer({
     originWhitelist: [
         'http://localhost:3000',
-        'https://too-collins-weather-app.herokuapp.com/'
+        'https://cors78.tipiku.my.id/'
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
